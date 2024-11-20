c3e81ac4485a5aa3aa41c7da506a8cb893738749
Andres Perez <perezandres1799@gmail.com>
2024-11-20 11:35:29 -0500

    Proyecto base

---
+++ src/main/java/com/example/PruebaNttData/PruebaNttDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.PruebaNttData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaNttDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaNttDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PruebaNttData/PruebaNttDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PruebaNttData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaNttDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
